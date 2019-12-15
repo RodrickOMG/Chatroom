637457e58c9d7e5dfedb219bc25862ce704066b1
Rodrick <206552@qq.com>
2019-12-15 23:37:26 +0800

    Initial commit

---
+++ client.py
@@ -0,0 +1,359 @@
+import socket
+import threading
+import json
+import time
+from tkinter import *
+from tkmacosx import Button
+
+HOST = '127.0.0.1'  # 主机地址
+PORT = 8888  # 端口号
+BUFFSIZE = 2048  # 缓存区大小，单位是字节，这里设定了2K的缓冲区
+ADDR = (HOST, PORT)  # 链接地址
+count = 0
+userlist = {}
+grouplist = {}
+
+
+class Client:
+    def __init__(self):
+        self.tcpCliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.isConnect = False
+
+    def connect(self):
+        """连接服务器"""
+        if not self.isConnect:
+            self.tcpCliSock.connect(ADDR)
+            self.isConnect = True
+            print("连接成功")
+        else:
+            print("已经连接，不再重新连接")
+
+    def relogin(self):
+        """重新加入聊天室"""
+        self.tcpCliSock.close()
+        self.__init__()
+
+    def disconnect(self):
+        """断开服务器"""
+        self.tcpCliSock.close()
+
+    @staticmethod
+    def error_msg(info):
+        """错误提示界面"""
+        errtk = Tk()
+        errtk.geometry('250x120')
+        errtk.title("加入聊天室失败")
+        frame = Frame(errtk, bg='#3C3F41')
+        frame.pack(expand=YES, fill=BOTH)
+        Label(frame, bg='#3C3F41', fg='#00FA92', text=info).pack(padx=5, pady=20, fill='x')
+        bt = Button(frame, text="确定", bg='#3C3F41', fg='#00FA92', command=errtk.destroy).pack()
+        errtk.mainloop()
+
+    class Login:
+        """登录界面"""
+
+        def __init__(self, parent):
+            self.parent = parent
+
+        def login(self, entry, login_window):
+            """登录"""
+            username = entry.get()
+            if username == "":
+                self.parent.error_msg("用户名不能为空")
+                return False
+            self.parent.username = username
+            data = {"type": "login", "username": username}
+            json_data = json.dumps(data)
+            try:
+                self.parent.connect()
+            except Exception as err:
+                print(err)
+                self.parent.error_msg("网络连接异常，无法连接到聊天室")
+                return False
+            else:
+                sock = self.parent.tcpCliSock
+                json_data = str.encode(json_data)
+                sock.send(json_data)
+                print('__send__' + str(json_data))
+                receive_json_data = sock.recv(BUFFSIZE)
+                receive_data = json.loads(receive_json_data, strict=False)
+                if receive_data["type"] == "login" and receive_data["username"] == username and \
+                        receive_data["status"] == True:
+                    # login success!
+                    print("进入聊天室成功")
+                    main_chat = self.parent.MainChat(self.parent, username)
+                    login_window.destroy()
+                    main_chat.__main__()
+                elif receive_data["type"] == "login" and receive_data["username"] == username \
+                        and receive_data["info"] == "该用户名已被占用":
+                    self.parent.relogin()
+                    self.parent.error_msg("该用户名已被占用")
+
+        def login_window(self):
+            """登录窗口"""
+            tk = Tk()
+            tk.geometry('350x200')
+            tk.title('登录')
+            frame = Frame(tk, bg='#3C3F41')
+            frame.pack(expand=YES, fill=BOTH)
+            Label(frame, bg='#3C3F41', fg="#00FA92", font="Arial, 15", text="请输入用户名：", anchor='w').pack(padx=20, pady=15, fill='x')
+            entry = Entry(frame, highlightthickness=0, highlightbackground='black', bg='#2B2B2B', fg='#00FA92', insertbackground='#00FA92', width=50, relief='flat')
+            entry.pack(padx=10, fill='x')
+            entry.bind("<Key-Return>", lambda x: self.login(entry, tk))
+            button = Button(frame, font="Arial, 15", bg='#3C3F41', fg='#00FA92', text="加入聊天室", command=lambda: self.login(entry, tk))
+            button.pack(pady=40)
+
+            tk.mainloop()
+
+        def __main__(self):
+            # 建立窗口
+            self.login_window()
+
+    class MainChat:
+        """聊天室主窗口"""
+
+        def __init__(self, parent, username):
+            self.parent = parent
+            self.socket = parent.tcpCliSock
+            self.username = username
+
+        class MainWindow:
+            def __init__(self, parent):
+                self.parent = parent
+
+            def main_window(self):
+                """聊天室主窗口"""
+                parent = self.parent
+                tk = Tk()
+                tk.geometry('600x450')
+                tk.title('Chat Room')
+
+                # 主背景
+                f = Frame(tk, bg='#3C3F41', width=600, height=450)
+                f.place(x=0, y=0)
+
+                # 欢迎语
+                text = StringVar()
+                text.set("欢迎来到聊天室, " + self.parent.username)
+                Label(f, fg="#00FA92", bg='#3C3F41', font="Courier, 14", textvariable=text).place(x=170, y=10, anchor=NW)
+
+                # 聊天的内容框
+                text_area = Text(f, highlightthickness=0, bg='#2B2B2B', fg="#00FA92", insertbackground='#00FA92', width=60, height=22, bd=0)
+                text_area.place(x=12, y=40, anchor=NW)
+                text_area.bind("<KeyPress>", lambda x: "break")
+                parent.text_area = text_area
+
+                # 右侧联系人选单
+                Label(f, fg="#00FA92", text="联系人:", bg="#3C3F41").place(x=455, y=10, anchor=NW)
+                listbox = Listbox(f, width=13, height=13, bg='#2B2B2B', fg="#00FA92")
+                listbox.place(x=460, y=40, anchor=NW)
+                parent.listbox = listbox
+
+                # 创建群聊
+                bt_create_group = Button(f, bg='#3C3F41', fg='#D636D2', width=100, text="创建多人聊天", font="Arial, 12", command=lambda: self.create_group_window())
+                bt_create_group.place(x=520, y=290, anchor=CENTER)
+
+                bt_clear = Button(f, bg='#3C3F41', fg='#DBBF6C', width=50, text="清屏", command=lambda: text_area.delete(0.0, END))
+                bt_clear.place(x=560, y=365, anchor=CENTER)
+
+                bt_pic = Button(f, bg='#3C3F41', fg='#D636D2', width=55, text="图片", font="Arial, 14", command=lambda: self.send(parent.socket, chat_name, et_input))
+                bt_pic.place(x=110, y=405, anchor=CENTER)
+
+                bt_file = Button(f, bg='#3C3F41', fg='#D636D2', width=55, text="文件", font="Arial, 14", command=lambda: self.send(parent.socket, chat_name, et_input))
+                bt_file.place(x=180, y=405, anchor=CENTER)
+
+                # 下方内容输入
+                chat_name = Label(f, fg="#00FA92", text="群聊", font="Arial, 12", bg='#2B2B2B', width=8)
+                chat_name.place(x=12, y=352)
+                listbox.bind('<Double-Button-1>', lambda x: self.change_send_to(listbox, chat_name))
+                self.chat_name = chat_name
+
+                # 输入要发送的消息
+                et_input = Entry(f, highlightthickness=0, highlightbackground='black', width=37, bg='#2B2B2B', fg='#00FA92', insertbackground='#00FA92', borderwidth=0)
+                et_input.place(x=90, y=355)
+                et_input.bind('<Key-Return>', lambda x: self.send(parent.socket, chat_name, et_input))
+                self.et_input = et_input
+
+                # 发送消息
+                bt_send = Button(f, bg='#3C3F41', fg='#4FB6F9', width=50, text="发送", command=lambda: self.send(parent.socket, chat_name, et_input))
+                bt_send.place(x=480, y=365, anchor=CENTER)
+
+                tk.mainloop()
+
+            def create_group_window(self):
+                """创建多人聊天"""
+                parent = self.parent
+                tk = Tk()
+                tk.geometry('200x300')
+                tk.title('创建多人聊天')
+
+                f = Frame(tk, bg='#3C3F41', width=200, height=300)
+                f.place(x=0, y=0)
+
+                listbox = Listbox(f, width=200, height=200, bg='#2B2B2B', fg="#00FA92", selectmode=MULTIPLE)
+                listbox.place(x=0, y=0, anchor=NW)
+                for l in userlist.keys():
+                    listbox.insert(END, l)
+                listbox.pack()
+
+                bt_confirm = Button(f, bg='#3C3F41', fg='#DBBF6C', width=50, text="确定", command=lambda: self.group_name(listbox, tk))
+                bt_confirm.place(x=50, y=265, anchor=CENTER)
+
+                bt_cancel = Button(f, bg='#3C3F41', fg='#DBBF6C', width=50, text="取消", command=tk.destroy)
+                bt_cancel.place(x=140, y=265, anchor=CENTER)
+
+                tk.mainloop()
+
+            def group_name(self, listbox, parent_tk):
+                """输入群聊名称"""
+                tk = Tk()
+                tk.geometry('350x200')
+                tk.title('输入群聊名称')
+                frame = Frame(tk, bg='#3C3F41')
+                frame.pack(expand=YES, fill=BOTH)
+                Label(frame, bg='#3C3F41', fg="#00FA92", font="Arial, 13", text="请输入群聊名称：", anchor='w').pack(padx=20,
+                                                                                                            pady=15,
+                                                                                                            fill='x')
+                entry = Entry(frame, highlightthickness=0, highlightbackground='black', bg='#2B2B2B', fg='#00FA92',
+                              insertbackground='#00FA92', width=50, relief='flat')
+                entry.pack(padx=10, fill='x')
+                entry.bind("<Key-Return>", lambda x: self.create_group(entry, tk, listbox, parent_tk))
+                button = Button(frame, font="Arial, 13", bg='#3C3F41', fg='#00FA92', text="创建群聊",
+                                command=lambda: self.create_group(entry, tk, listbox, parent_tk))
+                button.pack(pady=40)
+
+                tk.mainloop()
+
+            def change_send_to(self, listbox, chat_name):
+                """双击选择聊天对象"""
+                try:
+                    if self.parent.username != listbox.get(listbox.curselection()):
+                        chat_name['text'] = listbox.get(listbox.curselection())
+                except Exception as err:
+                    print(err)
+                    pass  # nothing choose
+
+            def create_group(self, entry, tk, listbox, parent_tk):
+                group_name = entry.get()
+                if group_name == "":
+                    self.parent.parent.error_msg("群聊名称不能为空")
+                else:
+                    tk.destroy()
+                    parent_tk.destroy()
+
+            def send(self, socket, chat_name, et_input):
+                """点击发送按钮"""
+                text = et_input.get()
+                to_user = chat_name['text']
+                username = self.parent.username
+                print(to_user)
+                if to_user == '群聊':
+                    data = {'type': 'group_chat', 'msg': text, 'username': username}
+                elif to_user == '组播':
+                    return
+                else:
+                    # 私聊
+                    data = {'type': 'private_chat', 'msg': text,
+                            'to': to_user, 'username': username}
+                    text_area = self.parent.text_area
+                    t = "[" + to_user + ']' + text + '\n'
+                    text_area.insert(END, t)
+                json_data = json.dumps(data)
+                json_data = str.encode(json_data)
+                socket.send(json_data)
+                print('__send__' + str(json_data))
+                et_input.delete(0, END)
+
+        class ListenThread(threading.Thread):
+            """Socket监听线程，对收到的信息作出相应反馈"""
+
+            def __init__(self, socket, parent):
+                threading.Thread.__init__(self)
+                self.parent = parent
+                self.socket = socket
+
+            def run(self):
+                while True:
+                    try:
+                        receive_json_data = self.socket.recv(BUFFSIZE)
+                        receive_data = json.loads(receive_json_data, strict=False)
+                    except Exception as err:
+                        print(err)
+                        break
+                    print("receive data: " + str(receive_data))
+                    switch = {
+                        "login": self.list,
+                        "remove_user": self.list,
+                        "group_chat": self.chat,
+                        "private_chat": self.chat,
+                        "pong": self.pong
+                    }
+                    switch[receive_data['type']](receive_data)
+                print("与服务器断开连接")
+
+            def list(self, data):
+                """刷新列表"""
+                global count, userlist
+                if data['type'] == 'login' and count < 1:
+                    time.sleep(0.1)
+                    if count == 0:
+                        text_area = self.parent.text_area
+                        text = (data['username'] + "加入聊天室\n")
+                        text_area.insert(END, text)
+                        userlist[data['username']] = {}
+                    data['type'] = 'init_list'
+                    json_data = json.dumps(data)
+                    json_data = str.encode(json_data)
+                    self.socket.send(json_data)
+                    print('send data: ' + str(json_data))
+                    count += 1
+                elif data['type'] == 'remove_user':
+                    text_area = self.parent.text_area
+                    text = (data['username'] + "退出聊天室\n")
+                    text_area.insert(END, text)
+                else:
+                    count = 0
+                    listbox = self.parent.listbox
+                    list = ['群聊', '组播']
+                    list += data['list']
+                    listbox.delete(0, END)  # 清空现有列表
+                    userlist.clear()
+                    for l in list:
+                        listbox.insert(END, l)  # 插入新列表
+                        if str(l) != '群聊' and str(l) != '组播':
+                            userlist[l] = {}
+
+            def chat(self, data):
+                """接收聊天信息并打印"""
+                text_area = self.parent.text_area
+                if data ['type'] == 'group_chat':
+                    text = ('[群聊]' + data['username'] + ': ' + data['msg'] + '\n')
+                else:
+                    text = ('[' + data['username'] + ']' + data['username'] + ': ' + data['msg'] + '\n')
+                text_area.insert(END, text)
+
+            def pong(self, data):
+                """ping pong!"""
+                textArea = self.parent.textArea
+                text = '[Server]pong\n'
+                textArea.insert(END, text)
+
+        def __main__(self):
+            # 开启监听线程
+            listenThread = self.ListenThread(self.socket, self)
+            listenThread.daemon = True
+            listenThread.start()
+            self.listenThread = listenThread
+
+            window = self.MainWindow(self)
+            window.main_window()
+            self.window = window
+
+    def __main__(self):
+        login = Client.Login(self)
+        login.__main__()
+
+
+if __name__ == '__main__':
+    client = Client()
+    client.__main__()
---
+++ server.py
@@ -0,0 +1,188 @@
+import socket
+import threading
+import json
+
+CHAT_SERVER_NAME = 'server'
+
+HOST = '127.0.0.1'  # 主机地址
+PORT = 8888  # 端口号
+BUFFSIZE = 2048  # 缓存区大小，单位是字节，这里设定了2K的缓冲区
+ADDR = (HOST, PORT)  # 链接地址
+
+
+# 公用函数
+def get_keys(list, value_list):
+    return [k for k, v in list.items() if v in value_list]
+
+
+class User:
+    def __init__(self, address, tcpCliSock):
+        self.address = address
+        self.tcpCliSock = tcpCliSock
+
+
+class Handle:
+    userlist = {}  # 用户列表,User对象
+    usernames = []  # 用户名列表
+
+    def __init__(self, user):
+        self.user = user
+
+    def login(self, data):
+        """处理登录信息包"""
+        if data["username"] in Handle.userlist.values() or self.user in Handle.userlist.keys():
+            data["status"] = False
+            data["info"] = "该用户名已被占用"
+        else:
+            data["status"] = True
+            Handle.userlist[self.user] = data["username"]
+            Handle.usernames.append(data["username"])
+            self.user.username = data["username"]
+            self.refresh_list(data)
+        self.send_socket_to_self(data)
+
+    def refresh_list(self, data):
+        """刷新在线用户列表"""
+        nameList = Handle.usernames
+        data["list"] = nameList
+        userlist = [user for user in Handle.userlist]
+        self.send_socket_to_all(userlist, data)
+
+    def init_list(self, data):
+        """获取在线用户列表"""
+        data["type"] = "login"
+        nameList = Handle.usernames
+        data["list"] = nameList
+        self.send_socket_to_self(data)
+
+    def group_chat(self, data):
+        """群聊"""
+        userlist = [user for user in Handle.userlist]
+        self.send_socket_to_all(userlist, data)
+
+    def private_chat(self, data):
+        """私聊"""
+        send_to_namelist = data['to']
+        send_to_userlist = get_keys(Handle.userlist, send_to_namelist)
+        self.send_socket_to_users(send_to_userlist, data)
+
+    @staticmethod
+    def send_socket_to_users(send_to_userlist, data):
+        """向用户列表发送信息包"""
+        json_data = json.dumps(data)
+        json_data = str.encode(json_data)
+        for user in send_to_userlist:
+            user.tcpCliSock.send(json_data)
+        print("Send to users: " + str(json_data))
+
+    @staticmethod
+    def send_socket_to_all(userlist, data):
+        """给所有用户发送信息包"""
+        json_data = json.dumps(data)
+        json_data = str.encode(json_data)
+        for user in userlist:
+            user.tcpCliSock.send(json_data)
+        print("Send to all users: " + str(json_data))
+
+    def send_socket_to_self(self, data):
+        """给本用户发送信息包"""
+        json_data = json.dumps(data)
+        json_data = str.encode(json_data)
+        self.user.tcpCliSock.send(json_data)
+        print('Send to user self: ' + str(json_data))
+
+    @staticmethod
+    def remove_user(user):
+        try:
+            handle = Handle(user)
+            Handle.userlist.pop(user)
+            Handle.usernames.remove(user.username)
+            data = {"type": "remove_user", "username": user.username}
+            handle.refresh_list(data)
+        except Exception as err:
+            print(err)
+
+    def __main__(self, data):
+        """处理信息包"""
+        type = data["type"]
+        switch = {
+            "login": self.login,
+            "init_list": self.init_list,
+            "group_chat": self.group_chat,
+            "private_chat": self.private_chat,
+        }
+        try:
+            switch[type](data)
+        except Exception as e:
+            print(e)
+            data["status"] = False
+            data["info"] = "未知错误"
+            return data
+
+
+class ClientThread(threading.Thread):
+    def __init__(self, user):
+        threading.Thread.__init__(self)
+        self.user = user
+
+    def run(self):
+        handle = Handle(self.user)  # handle input
+        try:
+            while True:
+                json_data = self.user.tcpCliSock.recv(BUFFSIZE)
+                data = json.loads(json_data, strict=False)
+                print("receive data from: " + data['username'])
+                if data['type'] == 'logout':
+                    break
+                else:
+                    handle.__main__(data)
+        except Exception as err:
+            print("连接中断")
+            print(err)
+        finally:
+            name = Handle.userlist[self.user]
+            print(str(name) + "退出聊天室")
+            Handle.remove_user(self.user)
+            self.user.tcpCliSock.close()
+
+    def stop(self):
+        try:
+            self.user.tcpCliSock.shutdown(2)
+            self.user.tcpCliSock.close()
+        except Exception as err:
+            print("连接中断")
+            print(err)
+
+
+class ChatServer:
+    def __main__(self):
+        tcpSerSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcpSerSock.bind(ADDR)
+        tcpSerSock.listen(5)
+
+        threads = []
+
+        while True:
+            try:
+                print('Waiting for connection...')
+                tcpCliSock, addr = tcpSerSock.accept()
+                print('...connected from:', addr)
+
+                user = User(addr, tcpCliSock)
+                clientThread = ClientThread(user)
+                threads += [clientThread]
+                clientThread.start()
+            except KeyboardInterrupt:
+                print('KeyboardInterrupt:')
+                for t in threads:
+                    t.stop()
+                break
+
+        tcpSerSock.close()
+
+
+if __name__ == '__main__':
+    print("\t\tChat Room Server\n")
+
+    server = ChatServer()
+    server.__main__()
